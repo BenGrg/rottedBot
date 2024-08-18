15b72cf6032db7e74fb242bf22ae323809073c1f
ben <rottedben@protonmail.com>
2024-08-18 19:39:16 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -29,9 +29,12 @@ const abiOracle = [
 ]
 
 const oracleAddy = "0x48dc32eca58106f06b41de514f29780ffa59c279"
+const maggotAddy = "0x163c754eF4D9C03Fc7Fa9cf6Dd43bFc760E6Ce89"
 
 const rotContract = new ethers.Contract(rotToken, abiErc20, provider);
+const maggotContract = new ethers.Contract(rotToken, abiErc20, provider);
 const oracleContract = new ethers.Contract(oracleAddy, abiOracle, provider);
+
 
 const TOKEN_DECIMALS = 18;
 
@@ -57,7 +60,7 @@ const lastMonth = 216000;
     });
     bot.command('mcap', async (ctx) => {
         console.log("/mcap")
-        const supply = await getTotalSupply()
+        const supply = await getTotalSupply(rotContract)
         const currentBlock = await provider.getBlockNumber()
         const price = await getPriceAt(currentBlock)
         ctx.replyWithHTML(`Current mcap: ${formatLargeNumber(price * supply, 0)}$`)
@@ -65,10 +68,10 @@ const lastMonth = 216000;
     bot.command('deflation', async (ctx) => {
         console.log("/deflation")
         const currentBlock = await provider.getBlockNumber()
-        const now = await getTotalSupply()
-        const nLastHour = await getTotalSupplyAt(currentBlock - lastHour)
-        const nLastDay = await getTotalSupplyAt(currentBlock - lastDay)
-        const nLastMonth = await getTotalSupplyAt(currentBlock - lastMonth)
+        const now = await getTotalSupply(rotContract)
+        const nLastHour = await getTotalSupplyAt(rotContract, currentBlock - lastHour)
+        const nLastDay = await getTotalSupplyAt(rotContract, currentBlock - lastDay)
+        const nLastMonth = await getTotalSupplyAt(rotContract, currentBlock - lastMonth)
         const str = `TOKEN'S BURNING 🥵: 
 Current supply: ${formatLargeNumber(now)}
 Last hour: ${formatLargeNumber(nLastHour)} = <b>${formatterPercentage.format((nLastHour - now) / nLastHour)}</b>
@@ -80,11 +83,11 @@ Last month: ${formatLargeNumber(nLastMonth)} = <b>${formatterPercentage.format((
     });
     bot.command('stuck', async (ctx) => {
         console.log("/stuck")
-        const totalSupply = await getTotalSupply()
-        const r = await Promise.all(contractsWhereTokensAreStuck.map(a => getBalance(a)))
+        const totalSupply = await getTotalSupply(maggotContract)
+        const r = await Promise.all(contractsWhereTokensAreStuck.map(a => getBalance(maggotContract, a)))
         const total = Number(r.reduce((a, b) => a + b) / BigInt(1e18));
         const percent = total / totalSupply
-        ctx.reply(`Total amount stuck: ${formatLargeNumber(total, 0)} ROT (${formatterPercentage.format(percent)})`)
+        ctx.reply(`Total maggot stuck: ${formatLargeNumber(total, 0)} MAGGOT (${formatterPercentage.format(percent)})`)
     });
 
     bot.launch({
@@ -94,11 +97,11 @@ Last month: ${formatLargeNumber(nLastMonth)} = <b>${formatterPercentage.format((
     console.log("Bot STARTED")
 })()
 
-async function getTotalSupplyAt(block: number) {
+async function getTotalSupplyAt(contract: any, block: number) {
     try {
         const blockTag: ethers.BlockTag = block
         // @ts-ignore
-        const totalSupply = await rotContract["totalSupply"].staticCall(...[], {blockTag});
+        const totalSupply = await contract["totalSupply"].staticCall(...[], {blockTag});
 
         return Number((totalSupply as bigint) / BigInt(1e18));
     } catch (error) {
@@ -120,10 +123,10 @@ async function getPriceAt(block: number) {
     }
 }
 
-async function getTotalSupply() {
+async function getTotalSupply(contract: any) {
     try {
         // @ts-ignore
-        const totalSupply = await rotContract.totalSupply();
+        const totalSupply = await contract.totalSupply();
 
         return Number((totalSupply as bigint) / BigInt(1e18));
     } catch (error) {
@@ -132,10 +135,10 @@ async function getTotalSupply() {
     }
 }
 
-async function getBalance(holderAddress: string) {
+async function getBalance(contract: any, holderAddress: string) {
     try {
         // @ts-ignore
-        const balance = await rotContract.balanceOf(holderAddress);
+        const balance = await maggotContract.balanceOf(holderAddress);
 
         return balance as bigint
     } catch (error) {
